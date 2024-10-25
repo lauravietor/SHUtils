24a4f55830fc6f6db74919b11cd787e377a772c9
Laura VIETOR <paul.vietor@centrale-med.fr>
2024-10-25 15:26:24 +0200

    Hunt detailed view skeleton

--- src/screens/hunts.rs
+++ src/screens/hunts.rs
@@ -1,11 +1,64 @@
 use crate::hunt::Hunt;
-use iced::widget::text;
-use iced::Element;
+use crate::pokemon::{GameVersion, Pokemon};
+use iced::alignment::Horizontal;
+use iced::widget::{button, column, container, row, stack, text};
+use iced::{Element, Length};
 
 #[derive(Debug, Clone, Copy)]
 pub enum HuntsMessage {
-    CreateHunt(usize),
+    CreateHunt,
     DeleteHunt(usize),
+    EditHunt(usize),
+}
+
+impl Hunt {
+    pub fn view_details(&self, id: usize) -> Element<HuntsMessage> {
+        let make_row = |label, value| {
+            row![
+                text(label)
+                    .size(14)
+                    .width(Length::Fill)
+                    .align_x(Horizontal::Right),
+                text(value).size(14).width(Length::Fill)
+            ]
+            .spacing(4)
+        };
+
+        stack![
+            container(
+                column![
+                    row![
+                        container(text("sprite here").width(100).height(100))
+                            .align_right(Length::Fill),
+                        column![
+                            text("Mew").size(20),
+                            text(self.count).size(24),
+                            text("Phase 1").size(16)
+                        ]
+                        .width(Length::Fill)
+                        .spacing(8)
+                    ]
+                    .spacing(8)
+                    .padding(16),
+                    column![
+                        make_row("Version :", "Émeraude"),
+                        make_row("Méthode :", "Fuites"),
+                        make_row("Zone :", "Île Loitaine"),
+                        make_row("Débutée le", "19 Juillet 2005"),
+                    ]
+                    .spacing(12)
+                    .padding(16)
+                ]
+                .spacing(8)
+                .padding(32)
+            ),
+            button("Edit")
+                .height(32)
+                .width(32)
+                .on_press(HuntsMessage::EditHunt(id))
+        ]
+        .into()
+    }
 }
 
 #[derive(Debug, Clone, Copy, Default)]
@@ -16,6 +69,13 @@ impl Hunts {
         Self {}
     }
     pub fn view(&self) -> Element<HuntsMessage> {
-        text("Hunts screen").into()
+        Hunt {
+            count: 3198,
+            pokemon: Pokemon {
+                species: 151,
+                version: GameVersion::Emerald,
+            },
+        }
+        .view_details(0)
     }
 }
